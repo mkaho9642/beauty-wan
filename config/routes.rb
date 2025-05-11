a2eb0463677446ecba80a9d8f642c0cde9a98e85
mkaho9642 <193476776+mkaho9642@users.noreply.github.com>
2025-05-11 16:24:57 +0000

    [Update,Add] controller etc..

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get 'home/about', to: 'homes#about', as: :about
+  resources :reviews, only: [:new, :create, :show]
 end
